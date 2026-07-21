2431a4d53b8ec183a81c35767338ccb64c15f595
Amro27code <amrosramrosr05@gmail.com>
2026-07-21 16:33:20 +0300

    onCreate Function

--- lib/sqlite/my_sqflite_database.dart
+++ lib/sqlite/my_sqflite_database.dart
@@ -4,19 +4,45 @@ import 'package:sqflite_department/sqlite/crud.dart';
 import 'package:path/path.dart' show join;
 
 class MySqfliteDatabase extends CRUD {
+  final String _userTable = "user";
+  final String _userIdColumn = "user_id";
+  final String _userNameColumn = "user_name";
+
+  final String _productTable = "product";
+  final String _productIdColumn = "product_id";
+  final String _productNameColumn = "product_name";
+  final String _productPriceColumn = "price";
+  final String _productCountColumn = "count";
+
+  final String _salesTable = "sales";
+  final String _salesIdColumn = "sales_id";
+  final String _salesProductNameColumn = "sales_product_name";
+  final String _salesUserNameColumn = "sales_user_name";
+
   void initDatabase() async {
     String path = await sqflite.getDatabasesPath();
-    //
+
     String managementDatabaseName = "management.db";
     String myPath = join(path, managementDatabaseName);
 
     int version = 1;
-    sqflite.openDatabase(
-      myPath,
-      version: version,
-      onCreate: (Database db, int version) {
+    sqflite.openDatabase(myPath, version: version, onCreate: _onCreate);
+  }
 
-      },
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE $_userTable"
+      " ($_userIdColumn INTEGER PRIMARY KEY,"
+      "$_userNameColumn TEXT);",
+    );
+    await db.execute(
+      "CREATE TABLE $_productTable ($_productIdColumn INTEGER PRIMARY KEY,$_productNameColumn TEXT,$_productPriceColumn REAL,$_productCountColumn INTEGER);",
+    );
+    await db.execute(
+      "CREATE TABLE $_salesTable"
+      " ($_salesIdColumn INTEGER PRIMARY KEY,"
+      "$_salesUserNameColumn TEXT,"
+      "$_salesProductNameColumn TEXT);",
     );
   }
 
